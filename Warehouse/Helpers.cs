287f4618ff8c83465dbe1f6d0cf032303642489e
slach12 <slach12@wp.pl>
2023-09-05 02:52:58 +0200

    Dodaj pliki projektów.

---
+++ Warehouse/Helpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse
+{
+    public class Helpers
+    {
+        public int GiveNumber()
+        {
+            SomeStructure stru = new SomeStructure();
+
+            int number = 5;
+            char character = 'i';
+            bool isTrue = false;
+            return number;
+        }
+
+        public enum ItemTypes
+        {
+            Grocery,
+            Clothing,
+            Electronics
+        }
+
+        public struct SomeStructure
+        {
+            public int numberForStructure;
+            private string nameOfStructure;
+            public SomeStructure(int number, string name)
+            {
+                this.numberForStructure = number;
+                this.nameOfStructure = name;
+            }    
+        }
+    }
+}
---
+++ Warehouse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Warehouse
+{
+    internal class Program
+    {
+
+        public const string FILE_NAME = "C:\\Warehouse\\ImportFile.xlsx";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to warehouse app!");
+            Console.WriteLine("Please let me know what you want to do:");
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Remove Item");
+            Console.WriteLine("3. Check Item");
+            Console.WriteLine("Press 1,2 or 3...");
+            
+            string choice = Console.ReadLine();
+            Console.WriteLine($"You have chosen option number : {choice}");
+
+            int chosenOption = 0;
+            Int32.TryParse(choice, out chosenOption );
+
+            Console.WriteLine("Please choose another option...");
+
+            choice = Console.ReadLine(); 
+        
+        }
+
+    }
+}
