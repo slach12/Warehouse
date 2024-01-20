ea639f98412e12a6dce0c339b70698d55e3636ff
slach12 <slach12@wp.pl>
2024-01-20 23:53:58 +0100

    Prace nad aplikacją do akatywności - projekt 1

---
+++ ActivityApp/ItemService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityApp
+{
+    public class ItemService
+    {
+        public List<Item> Items = new List<Item>();
+  
+    }
+}
--- ActivityApp/Program.cs
+++ ActivityApp/Program.cs
@@ -32,16 +32,65 @@ namespace ActivityApp
                 •	Zrobić zestawienie aktywności w zadanym okresie czasu
             */
 
+            /* Console.WriteLine("Witam w  Activty App!");
+             Console.WriteLine("Powiedz mi co chesz zrobić:");
+             Console.WriteLine("1. Dodać aktywność");
+             Console.WriteLine("2. Usunąć aktywność");
+             Console.WriteLine("3. Sprawdzić listę aktywności");
+             Console.WriteLine("Wybierz opcię 1,2 lub 3...");
+
+             string choice = Console.ReadLine();
+             int choiseOption = 0;
+             Int32.TryParse(choice, out choiseOption);*/
+
+            //Użytkownik zostanie przywitany
+            ////a. Stworzenie nowego ćwiczenia
+            ////b. Usunięcie ćwiczenia
+            ////c. Wyświetlenie listy ćwiczeń
+            ////d. Zwrócenie listy ćwiczeń o zadanym filtrze (nazwa kategorii)
+            /////a1. Najpierw dostanę wyboru kategorię ćwiczenia
+            /////a2. Zostanę poproszony o wprowadzenie wszystkich szczegułów
+            /////b1. Zostanę poproszony o id albo nazwę produktu
+            /////b2. Usunę ten produkt z listy produktów
+            /////
+            
             Console.WriteLine("Witam w  Activty App!");
             Console.WriteLine("Powiedz mi co chesz zrobić:");
-            Console.WriteLine("1. Dodać aktywność");
-            Console.WriteLine("2. Usunąć aktywność");
-            Console.WriteLine("3. Sprawdzić listę aktywności");
-            Console.WriteLine("Wybierz opcię 1,2 lub 3...");
 
-            string choice = Console.ReadLine();
-            int choiseOption = 0;
-            Int32.TryParse(choice, out choiseOption);
+            MenuActionService actionService = new MenuActionService();
+            actionService = Initialize(actionService);
+
+            var mainMenu = actionService.GetMenuActionsByMenuName("Menu");
+            for(int i = 0; i < mainMenu.Count; i++)
+            {
+                Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+            }
+            var operation = Console.ReadKey();
+            switch (operation.KeyChar)
+            {
+                case '1':
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                case '4':
+                    break;
+
+                default: 
+                    Console.WriteLine("Polecenie, które wybrałeś nie istnieje.");
+                    break;
+            }
+
+        }
+
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Dodaj aktywność", "Main");
+            actionService.AddNewAction(2, "Usuń aktwyność", "Main");
+            actionService.AddNewAction(3, "Pokaż detale aktywności", "Main");
+            actionService.AddNewAction(4, "Lista aktywności", "Main");
+            return actionService;
         }
     }
 }
